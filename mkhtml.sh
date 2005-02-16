1ea8896370be728c655ec2cbc4ce607952a68c94
tailor <tailor@f281.ttorni.ton.tut.fi>
2005-02-16 08:18:23 +0100

    Tailorization of trunk
    Import of the upstream sources from the repository
    
     http://tao.uab.es/ion/svn/ion-doc/trunk
    
    as of revision 80
    
    darcs-hash:20050216071823-0e4ef-57756ee71047a39040898140385db95be3ac7969.gz

---
+++ mkhtml.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+latex2html -split 4 -show_section_numbers -short_index -local_icons \
+-noaddress objects_and_extending -up_url http://www.iki.fi/tuomov/ion/ \
+-up_title "Ion homepage"
+
