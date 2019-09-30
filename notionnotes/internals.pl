baee78cd80e1b8c365caf767cf9df6bcab4a5708
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-30 09:09:51 +0000

    Deploy raboof/notion-doc to github.com/raboof/notion-doc.git:gh-pages

--- notionnotes/internals.pl
+++ notionnotes/internals.pl
@@ -2,32 +2,32 @@
 # Associate internals original text with physical files.
 
 
-$key = q/sec:exporting/;
-$ref_files{$key} = "$dir".q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:calling/;
-$ref_files{$key} = "$dir".q|node4.html|; 
+$key = q/par:parentship/;
+$ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/subsub:classhierarchy/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/par:managership/;
-$ref_files{$key} = "$dir".q|node2.html|; 
+$key = q/sec:calling/;
+$ref_files{$key} = "$dir".q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:objects/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/par:parentship/;
+$key = q/app:fullhierarchy/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/par:managership/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/app:fullhierarchy/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$key = q/sec:exporting/;
+$ref_files{$key} = "$dir".q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:supptypes/;
--- notionnotes/labels.pl
+++ notionnotes/labels.pl
@@ -2,14 +2,6 @@
 # Associate labels original text with physical files.
 
 
-$key = q/sec:exporting/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:calling/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/par:parentship/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
@@ -18,20 +10,28 @@ $key = q/subsub:classhierarchy/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/par:managership/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
+$key = q/sec:calling/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:objects/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sec:supptypes/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
+$key = q/par:managership/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/app:fullhierarchy/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:supptypes/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:exporting/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 1;
